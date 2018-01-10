9d6f25c641bcdb3af80cfd15941f4c1896a298e9
Dima1981 <dimyhametit@gmail.com>
2018-01-10 20:40:30 +0300

    anime

---
+++ pablic/main.js
@@ -0,0 +1,16 @@
+$("#link").click(function () {
+			$("div").animate(
+			{
+			opacity: 1,
+			marginRight: '90%'
+			}, 4000)
+		});
+
+
+$("#link-2").click(function () {
+			$("div").animate(
+			{
+			opacity: 0.10,
+			marginRight: "0%"
+			}, 5000);
+		});
